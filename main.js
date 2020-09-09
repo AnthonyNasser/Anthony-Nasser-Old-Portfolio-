848e5ea997ccd49293724191d08b15206628f194
AnthonyNasser <nasser.anthonyg@gmail.com>
2020-09-09 01:59:22 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function setFadeToggle(aTag) {
+  $(`a.${aTag}`).on("click", () => {
+    $(`.${aTag} .container`).fadeToggle(500, "linear");
+  });
+}
+function setAllToggles() {
+  setFadeToggle("about");
+  setFadeToggle("works");
+  setFadeToggle("toolkit");
+  setFadeToggle("contact");
+}
+function setUpButton() {
+  $("button.contact").on("click", () => {
+    $(".contact .container").focus();
+    if ($(".contact .container").css("display", "none")) {
+      $(".contact .container").fadeToggle();
+    }
+  });
+}
+function app() {
+  setUpButton();
+  setAllToggles();
+}
+
+app();
