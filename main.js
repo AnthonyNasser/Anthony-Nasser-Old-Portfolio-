87836edb2396d02450857c749fb021d84a816379
AnthonyNasser <nasser.anthonyg@gmail.com>
2021-04-11 17:12:08 -0700

    completed vanilla and jQuery carousel, added photos, set everything to open initially

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
+$(document).ready(function () {
+	openToggle('about')
+	openToggle('works')
+	openToggle('toolkit')
+	openToggle('contact')
+})
+
+function openToggle(aTag) {
+	$(`.${aTag} .container`).fadeToggle(500, 'linear')
+}
 function setFadeToggle(aTag) {
-  $(`a.${aTag}`).on("click", () => {
-    $(`.${aTag} .container`).fadeToggle(500, "linear");
-  });
+	$(`a.${aTag}`).on('click', () => {
+		$(`.${aTag} .container`).fadeToggle(500, 'linear')
+	})
 }
 function setAllToggles() {
-  setFadeToggle("about");
-  setFadeToggle("works");
-  setFadeToggle("toolkit");
-  setFadeToggle("contact");
+	setFadeToggle('about')
+	setFadeToggle('works')
+	setFadeToggle('toolkit')
+	setFadeToggle('contact')
 }
 function setUpButton() {
-  $("button.letsTalk").on("click", () => {
-    $(".contact .container").focus();
-    if ($(".contact .container").css("display", "none")) {
-      $(".contact .container").fadeToggle();
-    }
-  });
+	$('button.letsTalk').on('click', () => {
+		$('.contact .container').focus()
+		if ($('.contact .container').css('display', 'none')) {
+			$('.contact .container').fadeToggle()
+		}
+	})
 }
 function app() {
-  setUpButton();
-  setAllToggles();
+	setUpButton()
+	setAllToggles()
 }
 
-app();
+app()
+
+// Carousel Logic inspired by https://codepen.io/nopr/pen/rfBJx
+var carousel = $('.carousel'),
+	currdeg = 0
+
+$('.next').on('click', { d: 'n' }, rotate)
+$('.prev').on('click', { d: 'p' }, rotate)
+
+function rotate(e) {
+	if (e.data.d == 'n') {
+		currdeg = currdeg - 60
+	}
+	if (e.data.d == 'p') {
+		currdeg = currdeg + 60
+	}
+	carousel.css({
+		'-webkit-transform': 'rotateY(' + currdeg + 'deg)',
+		'-moz-transform': 'rotateY(' + currdeg + 'deg)',
+		'-o-transform': 'rotateY(' + currdeg + 'deg)',
+		transform: 'rotateY(' + currdeg + 'deg)',
+	})
+}
